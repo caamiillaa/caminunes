9d555324df05f15bb0d3e83b2b501621e77abad2
Camilla <camilla.santos@esharesinc.com>
2019-02-19 12:17:05 -0300

    testing new styles

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+	render() {
+		return (
+			<header className="header">
+				<p className="header--title">Hi, I'm Cami Nunes.</p>
+			</header>
+		);
+	}
+}
+
+export default Header;
